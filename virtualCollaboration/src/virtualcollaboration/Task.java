93d1c1284ae70626a0900336574c1e41bd05d1d4
Iqra <sp21bsse0021@maju.edu.pk>
2022-12-26 01:15:09 +0500

    Tasks updated

--- virtualCollaboration/src/virtualcollaboration/Task.java
+++ virtualCollaboration/src/virtualcollaboration/Task.java
@@ -7,42 +7,48 @@ import java.sql.Statement;
 
 public class Task {
 	
-	public String seeTask(int taskID) {
+	public String[] seeTask(int groupID) {
 		
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con =
 			DriverManager.getConnection("jdbc:mysql://localhost:3306/virtual_collaboration","root","");
 			Statement stmt=con.createStatement();
-			ResultSet rs=stmt.executeQuery("SELECT * from tasks where TaskID="+taskID);
-			String Required=null;
-			//ArrayList<String> Announcements = new ArrayList<String>();
+			ResultSet rs=stmt.executeQuery("SELECT * from tasks where GroupID="+groupID);
+			int size=0;
+			while(rs.next())
+			{size++;
+			}
+			rs.beforeFirst();
+			String[] Tasks=new String[size];
+			int i=0;
 			while(rs.next())
 			{
 			
-				Required="Task: " + rs.getString(2) + "Time: " + rs.getString(3) + " Status: "
+				Tasks[i]="Task: " + rs.getString(2) + "Time: " + rs.getString(3) + " Status: "
 			+ rs.getString(5)+ "Type: " + rs.getString(6);
+				i++;
 			}
 			con.close();
-			return Required;
+			return Tasks;
 			
 			} catch(Exception e)
 			{
-				
-				return e.getMessage();
+				String[] ex= {e.getMessage()};
+				return ex;
 			}
 		
 	}
 	
-	public String addTask(String task,String date,String time) {
+	public String addTask(String task,String date,String time,int GroupID) {
 		
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con =
 			DriverManager.getConnection("jdbc:mysql://localhost:3306/virtual_collaboration","root","");
 			Statement stmt=con.createStatement();
-			String query1="INSERT INTO `tasks` ( `Name`, `Date`, `Time`) VALUES "
-					+ "('"+task+"', '"+date+"', '"+time+"');";
+			String query1="INSERT INTO `tasks` ( `Name`, `Date`, `Time`,GroupID) VALUES "
+					+ "('"+task+"', '"+date+"', '"+time+"',"+GroupID+");";
 			stmt.executeUpdate(query1);
 			
 			con.close();
@@ -71,6 +77,49 @@ public class Task {
 			}
 	}
 	
+	
+	public String addSpTask(String task,String date,String time,String Email) {
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/virtual_collaboration","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT UserID from users where users.Email="+Email);
+			rs.next();
+			int Required=rs.getInt(1);
+			String query1="INSERT INTO `assignedtasks` ( `Name`, `Date`, `Time`,UserID) VALUES "
+					+ "('"+task+"', '"+date+"', '"+time+"',"+Required+");";
+			stmt.executeUpdate(query1);
+			
+			con.close();
+				return "Command Sent to User";
+			} catch(Exception e)
+			{
+				return e.getMessage();
+			}
+	}
+	
+	public void userApproval(int UserID) {
+		
 
-
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/virtual_collaboration","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT * from assignedtasks where Status='Pending' and UserID="+UserID);
+			rs.next();
+			int Required=rs.getInt(1);
+			String query1="";
+			stmt.executeUpdate(query1);
+			
+			con.close();
+			//	return "Command Sent to User";
+			} catch(Exception e)
+			{
+				//return e.getMessage();
+			}
+		
+	}
 }
